a4630fc1c56c71500483a7defb512c83d05335dd
Cristina Avornic <christina_avornic@yahoo.com>
2019-11-02 13:08:06 +0100

    pause added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+//import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter stopwatch',
       theme: ThemeData(
-        primarySwatch: Colors.cyan,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'My first stopwatch'),
+      home: MyHomePage(title: 'Stopwatch'),
     );
   }
 }
@@ -28,6 +28,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  StreamSubscription f;
   Stream<int> stream;
   int _minutes = 0, _hours = 0, _counter;
   String time = "00:00:00";
@@ -64,14 +65,22 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   changeState() {
-    stream = increment(Duration(seconds: 1));
-    stream.listen((_counter) => convertTime());
+    stream = increment();
+    f = stream.listen((_counter) => convertTime());
   }
 
-  Stream<int> increment(Duration interval) async* {
+  stopState() {
+    f.pause();
+  }
+
+  resumeState() {
+    f.resume();
+  }
+
+  Stream<int> increment() async* {
     _counter = 0;
     while (true) {
-      await Future.delayed(interval);
+      await Future.delayed(Duration(seconds: 1));
       yield _counter++;
     }
   }
@@ -86,44 +95,68 @@ class _MyHomePageState extends State<MyHomePage> {
             )),
       ),
       body: Center(
+          child: Container(
+        padding: EdgeInsets.all(35),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$time',
-              style: TextStyle(
-                fontSize: 70.0,
-                color: Colors.grey,
-                fontWeight: FontWeight.w600,
-              ),
+          children: [
+            Row(
+              children: <Widget>[
+                Text(
+                  '$time',
+                  style: TextStyle(
+                    fontSize: 70.0,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton.extended(
+                  onPressed: changeState,
+                  icon: Icon(Icons.play_arrow),
+                  label: Text('START'),
+                  backgroundColor: Colors.teal[400],
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25.0))),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton.extended(
+                  onPressed: stopState,
+                  icon: Icon(Icons.pause),
+                  label: Text('PAUSE'),
+                  backgroundColor: Colors.teal[400],
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25.0))),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton.extended(
+                  onPressed: null,
+                  icon: Icon(Icons.loop),
+                  label: Text('RESTART'),
+                  backgroundColor: Colors.teal[400],
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25.0))),
+                ),
+              ],
             ),
           ],
         ),
-      ),
-      /*floatingActionButton: FloatingActionButton.extended(
-        onPressed: changeState,
-        tooltip: 'Increment',
-        icon: Icon(Icons.play_arrow),
-        label: Text('START'),
-        backgroundColor: Colors.cyan,
-        foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25.0))),
-      ),*/
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: SpeedDial(
-          animatedIcon: AnimatedIcons.play_pause,
-          onOpen: changeState,
-          children: [
-            SpeedDialChild(
-                child: Icon(Icons.loop),
-                label: "Restart",
-                onTap: () => print("First")),
-            SpeedDialChild(
-                child: Icon(Icons.alarm),
-                label: "Timer",
-                onTap: () => print("Second"))
-          ]),
+      )),
     );
   }
 }
